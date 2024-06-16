c4c2ef658cf6c8848ec9f6c0bd02aa7202c15724
Hacene <hacene.kasdi@caprikorn.com>
2024-06-16 17:01:22 +0200

    Initialize project

---
+++ src/test/java/io/moovewave/business/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package io.moovewave.business;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
